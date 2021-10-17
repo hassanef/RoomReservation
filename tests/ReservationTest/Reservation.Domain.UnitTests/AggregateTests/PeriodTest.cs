edd14c1ffe654cb5ee5c3349d290403e8b2062d9
Hassan Ebrahimi <hassan.ef@gmail.com>
2021-10-17 11:28:36 +0330

    changed periodtest based on period.

--- tests/ReservationTest/Reservation.Domain.UnitTests/AggregateTests/PeriodTest.cs
+++ tests/ReservationTest/Reservation.Domain.UnitTests/AggregateTests/PeriodTest.cs
@@ -3,8 +3,6 @@ using Reservation.Domain.AggregatesModel.RoomAggregate;
 using Reservation.Domain.AggregatesModel.RoomReservationAggregate;
 using Reservation.Domain.Exceptions;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using Xunit;
 
 namespace Reservation.Domain.UnitTests.AggregateTests
@@ -12,7 +10,7 @@ namespace Reservation.Domain.UnitTests.AggregateTests
     public class PeriodTest
     {
         [Fact]
-        public void Create_RoomReservaion_With_Valid_DateTime_For_Amsterdam()
+        public void Create_Period_With_Valid_DateTime_For_Amsterdam()
         {
             var period = Period.Create(GetValidStartDateTime(), GetValidEndDateTime(), Location.Amsterdam);
 
@@ -21,15 +19,41 @@ namespace Reservation.Domain.UnitTests.AggregateTests
             roomReservation.Should().NotBeNull();
         }
         [Fact]
-        public void Create_RoomReservaion_With_Valid_DateTime_For_Berlin()
+        public void Create_Period_With_Valid_DateTime_For_Berlin()
         {
             var period = Period.Create(GetValidStartDateTime(), GetValidEndDateTime_ForBerlin(), Location.Berlin);
 
-            var roomReservation = new RoomReservation(1, 1, period, Location.Berlin);
-
-            roomReservation.Should().NotBeNull();
+            period.Should().NotBeNull();
         }
- 
+        [Fact]
+        public void Create_Period_With_InValid_StartDateTime_For_Amsterdam()
+        {
+            Action act = () => { var period = Period.Create(GetInValidStartDateTime(), GetInValidEndDateTime_ForAmsterdam(), Location.Amsterdam); };
+
+            act.Should().Throw<ReservationException>();
+        }
+        [Fact]
+        public void Create_Period_With_InValid_StartDateTime_For_Berlin()
+        {
+            Action act = () => { var period = Period.Create(GetInValidStartDateTime(), GetInValidEndDateTime_ForBerlin(), Location.Berlin); };
+
+            act.Should().Throw<ReservationException>();
+        }
+        [Fact]
+        public void Create_Period_With_InValid_EndDateTime_For_Berlin()
+        {
+            Action act = () => { var period = Period.Create(GetValidStartDateTime(), GetInValidEndDateTime_ForBerlin(), Location.Berlin); };
+
+            act.Should().Throw<ReservationException>();
+        }
+        [Fact]
+        public void Create_Period_With_InValid_EndDateTime_For_Amsterdam()
+        {
+            Action act = () => { var period = Period.Create(GetValidStartDateTime(), GetInValidEndDateTime_ForAmsterdam(), Location.Amsterdam); };
+
+            act.Should().Throw<ReservationException>();
+        }
+
         private DateTime GetValidStartDateTime()
         {
             DateTime nowDate = DateTime.Now;
@@ -51,6 +75,30 @@ namespace Reservation.Domain.UnitTests.AggregateTests
             DateTime nowDate = DateTime.Now;
             DateTime endDate = new DateTime(nowDate.Year, nowDate.Month, nowDate.Day + 1,
                 19, 59, 0);
+
+            return endDate;
+        }
+        private DateTime GetInValidStartDateTime()
+        {
+            DateTime nowDate = DateTime.Now;
+            DateTime startDate = new DateTime(nowDate.Year, nowDate.Month, nowDate.Day + 1,
+                07, 59, 0);
+
+            return startDate;
+        }
+        private DateTime GetInValidEndDateTime_ForAmsterdam()
+        {
+            DateTime nowDate = DateTime.Now;
+            DateTime endDate = new DateTime(nowDate.Year, nowDate.Month, nowDate.Day + 1,
+                17, 01, 0);
+
+            return endDate;
+        }
+        private DateTime GetInValidEndDateTime_ForBerlin()
+        {
+            DateTime nowDate = DateTime.Now;
+            DateTime endDate = new DateTime(nowDate.Year, nowDate.Month, nowDate.Day + 1,
+                20, 01, 0);
 
             return endDate;
         }
