c1a86b9bc5da75694478cde63748a58dec8933b3
Hassan Ebrahimi <hassan.ef@gmail.com>
2021-10-25 15:52:44 +0330

    cleaning startup.

--- src/Identity/Identity.Api/Startup.cs
+++ src/Identity/Identity.Api/Startup.cs
@@ -2,24 +2,13 @@ using Identity.Common.ViewModels;
 using Identity.DataLayer.Context;
 using Identity.Services.Configuration;
 using Identity.Services.Token;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Identity.Api
 {
